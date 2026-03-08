af85e32d963d5b76856372738f8023ce59a967ef
Asbjorn-byte <thorndalkofod@gmail.com>
2026-03-08 15:54:54 +0100

    bug fix

--- src/tekpy/__init__.py
+++ src/tekpy/__init__.py
@@ -1,3 +1,11 @@
 from .circuit import polar_deg, polar_rek, rek_polar
 from .econ import DifferensMetoden, TotalMetoden
 from .workflow import TexTree
+
+__all__ = ["TexTree",
+           'polar_rek',
+           'polar_deg',
+           'rek_polar',
+           'DifferensMetoden',
+           'TotalMetoden',
+           ]
